5df41b170f2da901f4b4105c59310ec85c5aa193
danielfcollier <danielfcollier@gmail.com>
2022-11-21 13:33:26 -0300

    MQ Publisher/Subscriber.

---
+++ 03-mq-pub-sub/receiveLogs.js
@@ -0,0 +1,43 @@
+const amqp = require("amqplib/callback_api");
+
+amqp.connect("amqp://localhost", (error0, connection) => {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel((error1, channel) => {
+    if (error1) {
+      throw error1;
+    }
+    const exchange = "logs";
+
+    channel.assertExchange(exchange, "fanout", {
+      durable: false,
+    });
+
+    channel.assertQueue(
+      "",
+      {
+        exclusive: true,
+      },
+      (error2, q) => {
+        if (error2) {
+          throw error2;
+        }
+        console.log(` [*] Waiting for messages in ${q.queue}. To exit press CTRL+C`);
+        channel.bindQueue(q.queue, exchange, "");
+
+        channel.consume(
+          q.queue,
+          (msg) => {
+            if (msg.content) {
+              console.log(` [x] ${msg.content.toString()}`);
+            }
+          },
+          {
+            noAck: true,
+          }
+        );
+      }
+    );
+  });
+});
